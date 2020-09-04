73e8ff4a93c0b9779f8cb7d72673c9728bb6239a
Margarita Morozova <ritamorozova@yahoo.com>
2020-09-03 19:33:40 -0700

    Done.

--- run.rb
+++ run.rb
@@ -1,10 +1,16 @@
-require_relative "lib/Manager"
-require_relative "lib/Employee"
+require_relative "lib/Manager.rb"
+require_relative "lib/Employee.rb"
 require 'pry'
 
 
 #Test your code here
+bob = Manager.new("Bob", "Printing Press", 26)
+tina = Manager.new("Tina", "Accounting", 40)
 
+
+e1 = Employee.new("Anna", 45000, bob)
+e2 = Employee.new("Tom", 29000, bob)
+e3 = Employee.new("Dina", 30000, tina)
 
 binding.pry
 puts "done"
